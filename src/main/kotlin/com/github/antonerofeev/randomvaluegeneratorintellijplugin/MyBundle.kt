8d17eb6f25fe3a33fea5849f5627b0be50ee0c28
GitHub Action <action@github.com>
2024-02-07 20:20:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/antonerofeev/randomvaluegeneratorintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/antonerofeev/randomvaluegeneratorintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.antonerofeev.randomvaluegeneratorintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
